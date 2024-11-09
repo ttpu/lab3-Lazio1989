d97ea63a132c6c6af39894af57d3b4b0c4602d02
Lazio1989 <36448477+Lazio1989@users.noreply.github.com>
2024-11-09 14:48:17 +0500

    Update task1.ino
    Lab3

--- task1/task1.ino
+++ task1/task1.ino
@@ -1,10 +1,22 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #define btn 33
 #define red 4
 #define green 5
 #define yellow 19
 #define blue 22 
+
+// Wireless connection data
+const char* ssid = "#Turin.uz";
+const char* pass = "Turin_2024@!";
+const char* wifi_status = "Trying to connect";
+int count_wifi = 0;
+
+WiFiClient wifi_client;
+PubSubClient mqtt_client (wifi_client);
+
 
 void setup() {
   //defining button input
@@ -17,10 +29,60 @@ void setup() {
   pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(blue, OUTPUT);
+
+  // WIFI connection
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,pass);
+  delay(100);
+ 
+  //check wifi connected
+  while (WiFi.status() != WL_CONNECTED){
+    count_wifi ++;
+    Serial.println(wifi_status);
+    Serial.println(count_wifi);
+    delay(1000);
+ 
+  }
+  count_wifi=0;
+  Serial.println("WiFi Connected"); 
+  
+  Serial.println("IP: "); 
+  Serial.println(WiFi.localIP()); 
+  
+  Serial.println("MAC: "); 
+  Serial.println(WiFi.macAddress()); 
+
+  mqtt_client.setServer("mqtt.iotserver.uz", 1883);
+  mqtt_client.setCallback(my_callback);
+
 }
 
 void loop() {
-  
+  while (!mqtt_client.connected()){
+    String macAddr = WiFi.macAddress();
+    String macSuffix = macAddr.substring(macAddr.length() - 5);
+    macSuffix.replace(":", "");
+
+    String myID = "edacs_lab4";
+    myID += macSuffix;
+
+    int mres = mqtt_client.connect(myID.c_str(),"userTTPU", "mqttpass");
+      if (mres){
+        Serial.print("mqtt connected: ");
+        Serial.print(mres);
+        mqtt_client.subscribe("ttpu/edacs/lab4");
+        mqtt_client.publish("ttpu/edacs/msg", "hello");
+      }
+      else {
+        Serial.print("mqtt Not connected, trying: ");
+        Serial.print(mres);
+        delay(3000);
+      }
+
+  }
+
+  mqtt_client.loop();
+
   static int count;
   
   static int prev_btn = 0;
@@ -57,4 +119,7 @@ void loop() {
   delay(100);
 
 
+}
+void my_callback (char* topic, byte* payload,  unsigned int len){
+  Serial.println("Msg recieved");
 }
